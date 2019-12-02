982800d81d78990937e9bae2cecb9403f5ed9228
NAHVUSdev <trentonenter@outlook.com>
2019-12-02 11:53:32 -0600

    additions to routes

--- index.js
+++ index.js
@@ -6,4 +6,28 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/app/static/index.html'));
 });
 
+app.get('/health', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/health/index.html'));
+});
+
+app.get('/lifestyle', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/lifestyle/index.html'));
+});
+
+app.get('/welcome', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/static/welcome.html'));
+});
+
+app.get('/incomplete', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/static/incomplete.html'));
+});
+
+app.get('/reviews', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/reviews/index.html'));
+});
+
+app.get('/account', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/user/account.html'));
+});
+
 app.listen(8080);
