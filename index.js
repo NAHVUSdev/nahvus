3645106c567ffe1fabe306791ed69c9d4676e6ac
NAHVUSdev <trentonenter@outlook.com>
2019-12-02 11:39:34 -0600

    fixed errors

--- index.js
+++ index.js
@@ -2,49 +2,28 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function (req, res) {
-    "use strict";
-    res.sendFile('index.html', {root: path.join(__dirname, 'app/static/')});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/static/index.html'));
 });
 
-app.get('/lifestyle', function (req, res) {
-    "use strict";
-    res.sendFile('index.html', {root: path.join(__dirname, 'app/lifestyle/')});
+app.get('/lifestyle', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/lifestyle/index.html'));
 });
 
-app.get('/health', function (req, res) {
-    "use strict";
-    res.sendFile('index.html', {root: path.join(__dirname, 'app/health/')});
+app.get('/health', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/health/index.html'));
 });
 
-app.get('/reviews', function (req, res) {
-    "use strict";
-    res.sendFile('index.html', {root: path.join(__dirname, 'app/reviews/')});
+app.get('/reviews', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/reviews/index.html'));
 });
 
-app.get('/user/account', function (req, res) {
-    "use strict";
-    res.sendFile('account.html', {root: path.join(__dirname, 'app/user/')});
+app.get('/welcome', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/static/welcome.html'));
 });
 
-app.get('/welcome', function (req, res) {
-    "use strict";
-    res.sendFile('welcome.html', {root: path.join(__dirname, 'app/static/')});
-});
-
-app.get('/incomplete', function (req, res) {
-    "use strict";
-    res.sendFile('incomplete.html', {root: path.join(__dirname, 'app/static/')});
-});
-
-app.get('/redir_nhvsasi', function (req, res) {
-    "use strict";
-    res.redirect('https://nahvusasi.com');
-});
-
-app.get('/redir_newtasi', function (req, res) {
-    "use strict";
-    res.redirect('https://newtonasi.com');
+app.get('/incomplete', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/static/incomplete.html'));
 });
 
 app.listen(8080);
